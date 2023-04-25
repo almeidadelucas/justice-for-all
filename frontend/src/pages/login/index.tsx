c1b65ddcc6ec31e68a1f3bace14350b2f557368e
Lucas de Almeida <ra248386@students.ic.unicamp.br>
2023-04-25 19:45:00 -0300

    Updating regex

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -16,7 +16,7 @@ const Form = styled.form`
 `;
 
 const EMAIL_REGEX =
-	/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export default function Login() {
 	const { push } = useRouter();
