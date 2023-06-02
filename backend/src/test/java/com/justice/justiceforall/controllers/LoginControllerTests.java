d5e48b1055e805d2fd300cd4bdf2d1398438256d
Jonathan do Ouro <j248364@dac.unicamp.br>
2023-06-02 20:53:17 -0300

    muda a pasta do teste de login

--- backend/src/test/java/com/justice/justiceforall/controllers/LoginControllerTests.java
+++ backend/src/test/java/com/justice/justiceforall/controllers/LoginControllerTests.java
@@ -1,8 +1,7 @@
-package com.justice.justiceforall.controller;
+package com.justice.justiceforall.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import com.justice.justiceforall.controllers.LoginController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
