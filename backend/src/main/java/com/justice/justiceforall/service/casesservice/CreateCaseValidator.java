60ea2c67c27b8d1c5e4c3e5d3b4ae0b4f1696050
Jonathan do Ouro <j248364@dac.unicamp.br>
2023-06-04 22:31:50 -0300

    mudifica os diretorios do projeto

--- backend/src/main/java/com/justice/justiceforall/service/casesservice/CreateCaseValidator.java
+++ backend/src/main/java/com/justice/justiceforall/service/casesservice/CreateCaseValidator.java
@@ -1,10 +1,17 @@
-package com.justice.justiceforall.service.casesservice;
+package com.justice.justiceforall.service.casesservice.casevalidator;
 
 import com.justice.justiceforall.dto.casesdto.CreateCaseCommand;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class CreateCaseValidator {
-
-    public static void validateNewCase(CreateCaseCommand createCaseCommand) {
+	void validateNewCase(CreateCaseCommand createCaseCommand) {
+        var fileFormatValidator = new FileFormatValidator();
+        var mustHaveFields = new MustHaveFields();
+        
+        fileFormatValidator.setNext(mustHaveFields);
+     
+        fileFormatValidator.validate(createCaseCommand);
     }
-
 }
