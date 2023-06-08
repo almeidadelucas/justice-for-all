79892d508ab970a6e732af4f1d9dac9796af4ca7
Lucas de Almeida <almeidadelucas@gmail.com>
2023-06-07 23:38:18 -0300

    Using hooks to simplify validation

---
+++ frontend/src/hooks/useValidateEmail.ts
@@ -0,0 +1,11 @@
+import { IFormValues } from '@/components/molecules/SignupForm';
+import { UseFormRegister } from 'react-hook-form';
+
+export function useValidateEmail(register: UseFormRegister<IFormValues>) {
+	return {
+		emailInputProps: register('email', { pattern: {
+			value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+			message: 'Email deve seguir o formato "email@email.com"'
+		}})
+	};
+}
---
+++ frontend/src/hooks/useValidateFirstName.ts
@@ -0,0 +1,17 @@
+import { IFormValues } from '@/components/molecules/SignupForm';
+import { UseFormRegister } from 'react-hook-form';
+
+export function useValidateFirstName(register: UseFormRegister<IFormValues>) {
+	return {
+		firstNameInputProps: register('firstName', {
+			minLength: {
+				value: 2,
+				message: 'O nome deve ter no mínimo 2 caracteres'
+			},
+			maxLength: {
+				value: 30,
+				message: 'O nome deve ter no máximo 30 caracteres'
+			}
+		})
+	};
+}
