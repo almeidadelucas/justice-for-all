8037893e65b3b76063aab88f6fcc9fcb9e58375a
Jonathan do Ouro <j248364@dac.unicamp.br>
2023-06-19 13:04:18 -0300

    atualzia o forms dos casos

--- frontend/src/components/molecules/CreateCaseForm.tsx
+++ frontend/src/components/molecules/CreateCaseForm.tsx
@@ -1,111 +1,161 @@
 import React, { FormEvent } from 'react';
 import { useState } from 'react';
-import { TextField, Box, Button } from '@mui/material';
+import { TextField, Button, Grid, Select, InputLabel, MenuItem, SelectChangeEvent } from '@mui/material';
 import axios from 'axios';
-import { Form } from '../atoms/Form';
+import { PickerOverlay } from 'filestack-react'
+
+const API_KEY = "AqWWD4a3yTJ2Ln1d9RX3cz";
+
+interface createCaseCommand {
+	title: string;
+	category: string;
+  description: string;
+  alegation: string;
+  evidencesPDF: string;
+  caseIdentifier: string;
+}
 
 export function CreateCaseForm() {
-  const [userId, setUserId] = useState('');
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
-  const [description, setDescription] = useState('');
-  const [allegation, setAllegation] = useState('');
-  const [evidencesPDF, setEvidencesPDF] = useState('');
-  const [evidenceImage, setEvidenceImage] = useState('');
-  const [caseIdentifier, setCaseIdentifier] = useState('');
-  const [open, setOpen] = useState(false);
+
+  const [caseData, setCase] = useState<createCaseCommand>({
+    title: '',
+    category: '',
+    description: '',
+    alegation: '',
+    evidencesPDF: '',
+    caseIdentifier: '',
+  });
+
+  const [erros, setErrors] = useState<createCaseCommand>({
+    title: '',
+    category: '',
+    description: '',
+    alegation: '',
+    evidencesPDF: '',
+    caseIdentifier: '',
+  });
+
+  const validate = () => {
+    let isValid: boolean = true;
+    const errors: Partial<createCaseCommand> = {};
+  
+    if (caseData.title.length < 5) {
+      errors.title = 'O título é obrigatório e deve ter mais de 5 caracteres.';
+      isValid = false;
+    }
+  
+    if (caseData.category.length < 5) {
+      errors.category = 'A categoria é obrigatória e deve ter mais de 5 caracteres.';
+      isValid = false;
+    }
+  
+    if (caseData.description.length < 20) {
+      errors.description = 'A descrição é obrigatória e deve ter mais de 20 caracteres.';
+      isValid = false;
+    }
+  
+    if (caseData.caseIdentifier === '') {
+      errors.caseIdentifier = 'O caso precisa ter um identificador da justiça brasileira, normalmente é encontrado na abertura do processo.';
+      isValid = false;
+    }
+  
+    setErrors((prevErrors) => ({ ...prevErrors, ...errors }));
+    return isValid;
+  };
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string>) => {
+    const fieldName =  event.target.name;
+    const fieldValue = event.target.value;
+    setCase(current => ({
+      ...current,
+      [fieldName]: fieldValue
+    }));
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const createCaseCommand = {
-      userId,
-      title,
-      category,
-      description,
-      allegation,
-      evidencesPDF,
-      evidenceImage,
-      caseIdentifier,
-      open,
-    };
-
-    axios
-      .post('http://localhost:8080/case', createCaseCommand)
-      .then((response) => {
-        // Process the successful response here
-        console.log('Case created successfully:', response.data);
-      })
-      .catch((error) => {
-        // Handle the error here
-        alert("api error")
-        console.error('Error creating case:', error);
-      });
+    if(validate()) {
+      console.log(caseData);
+    }else {
+      alert("?????/")
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          rowGap: '0.8rem',
-          width: '80v'
-        }}
-      >
-        <TextField
-          label="Titulo"
-          required
-          variant="standard"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-        <TextField
-          label="Categoria"
-          required
-          variant="standard"
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-        />
-        <TextField
-          label="Descrição"
-          required
-          variant="standard"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        />
-        <TextField
-          label="Alegação"
-          required
-          variant="standard"
-          value={allegation}
-          onChange={(event) => setAllegation(event.target.value)}
-        />
-        <TextField
-          label="Evidencias PDF"
-          required
-          variant="standard"
-          value={evidencesPDF}
-          onChange={(event) => setEvidencesPDF(event.target.value)}
-        />
-        <TextField
-          label="Evidencias Image"
-          required
-          variant="standard"
-          value={evidenceImage}
-          onChange={(event) => setEvidenceImage(event.target.value)}
-        />
-        <TextField
-          label="Case Identifier"
-          required
-          variant="standard"
-          value={caseIdentifier}
-          onChange={(event) => setCaseIdentifier(event.target.value)}
-        />
-      </Box>
-      <Button type="submit" variant="outlined">
-        Create Case
-      </Button>
-    </Form>
-  );
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="Título"
+              name = "title"
+              onChange={handleChange}
+              error={!!erros.title}
+				      helperText={erros.title}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="Categoria"
+              name='category'
+              onChange={handleChange}
+              error={!!erros.category}
+				      helperText={erros.category}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="Descrição"
+              name='description'
+              onChange={handleChange}
+              error={!!erros.description}
+				      helperText={erros.description}
+              fullWidth
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <InputLabel id="alegacao-label">Alegação</InputLabel>
+            <Select
+              labelId="alegacao-label"
+              id="alegacao-select"
+              name='alegation'
+              value='INNOCENT'
+              onChange={handleChange}
+              fullWidth
+            >
+              <MenuItem value="INNOCENT">Inocente</MenuItem>
+              <MenuItem value="GUITY">Culpado</MenuItem>
+            </Select>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="Identificador do caso"
+              name='caseIdentifier'
+              onChange={handleChange}
+              error={!!erros.caseIdentifier}
+				      helperText={erros.caseIdentifier}
+              fullWidth
+              multiline
+              rows={1}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button onClick={() => alert('teste')}>Open File</Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary">
+              Enviar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    );
 }
